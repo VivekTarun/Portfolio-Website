58a4e590a2f1395897746dca0cbb3306ad94c390
vivektarun <vivektarun1234@gmail.com>
2023-01-20 01:21:43 +0530

    Introduction part is done only image is left.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./App.css"
+import Intro from "./Components/introduction/Intro"
+
+
+const App = () => {
+  return (
+    <div>
+      <Intro />
+    </div>
+
+  )
+}
+
+export default App
